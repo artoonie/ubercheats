e215a34295e491efc553cbe59ba36ea86ed9f914
Armin Samii <armin.samii@gmail.com>
2020-08-12 19:09:44 -0400

    Automatically run and update status icon

--- background.js
+++ background.js
@@ -1,13 +1,158 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
+var isGoogleAPILoaded = false;
+
+function googleApiIsLoaded() {
+  isGoogleAPILoaded = true;
+}
+
+function addScriptTagToHead() {
+  // Create the script tag, set the appropriate attributes
+  var script = document.createElement('script');
+  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyA3ZbHa1nT0-WgfiY6HG11Lw2JhT4q3nFA&callback=googleApiIsLoaded';
+  script.defer = true;
+  
+  // Append the 'script' element to 'head'
+  document.head.appendChild(script);
+}
+
+function setIcon(iconName, tabId) {
+  chrome.pageAction.setIcon({
+      path: "icons/" + iconName,
+      tabId: tabId
   });
+}
+
+// Sets the status using the storage API to pass it to the popup
+function _setStatus(className, text) {
+  let status = {className: className, text: text};
+  chrome.storage.sync.set({popupStatus: status});
+}
+
+function setError(errorMessage) {
+  text = "Encountered an error.<br/>";
+  text += errorMessage;
+  text += "Please contact the developer at team@rcvis.com/*TODO*/ to address this.";
+  _setStatus('warning', text)
+}
+
+function setInfo(message) {
+  _setStatus('info', message)
+}
+
+function setAcceptable(message, tabId) {
+  setIcon("acceptable.png", tabId);
+  _setStatus('acceptable', message)
+}
+
+function setCheated(message, tabId) {
+  setIcon("cheated.png", tabId);
+  _setStatus('cheated', message)
+}
+
+function getLatLonFor(pinImageSource, googleMapsImageSource) {
+  var numberRegex = "[-]?[0-9]*"
+  var latOrLonRegex = "(" + numberRegex + "." + numberRegex + ")"
+  var latAndLonRegex = latOrLonRegex + "%2C" + latOrLonRegex
+  var pickupRegex = new RegExp(pinImageSource + "%7Cscale%3A2%7C" + latAndLonRegex, "g");
+  var match = pickupRegex.exec(googleMapsImageSource)
+  var pickupLatitude = match[1]
+  var pickupLongitude = match[2]
+  return [pickupLatitude, pickupLongitude]
+}
+
+function queryGoogleForDistance(startLatLon, endLatLon, uberPaidForDistance, tabId) {
+  let directionsService = new google.maps.DirectionsService();
+  let start = new google.maps.LatLng(parseFloat(startLatLon[0]), parseFloat(startLatLon[1]));
+  let end = new google.maps.LatLng(parseFloat(endLatLon[0]), parseFloat(endLatLon[1]));
+
+  setInfo("Reaching out to Google to compute the distance between " + start + " and " + end);
+
+  const route = {
+    origin: start,
+    destination: end,
+    travelMode: 'DRIVING'
+  }
+
+  directionsService.route(route, function(response, status) {
+    callbackDirectionsComplete(response, status, uberPaidForDistance, tabId);
+  });
+}
+
+function compareDistances(actualDistance, uberPaidForDistance, tabId) {
+  var mileageRegex = new RegExp("([0-9]*[.][0-9]*) (mi|km)", "g");
+  
+  // Get the uber match
+  var uberMatch = mileageRegex.exec(uberPaidForDistance);
+  // Reset regex
+  mileageRegex.lastIndex = 0;
+  // Get the actual match
+  var actualMatch = mileageRegex.exec(actualDistance);
+
+  // Error handling: This shouldn't happen.
+  if (!actualMatch || !uberMatch || actualMatch.length < 2 || uberMatch.length < 2)
+  {
+    setError("Could not parse mileages:\n" +
+             "\nactual=" + actualDistance +
+             "\nuber paid for=" + uberPaidForDistance +
+             "\nactual match=" + actualMatch +
+             "\nuber match=" + uberMatch);
+    return;
+  }
+
+  var actualFloat = parseFloat(actualMatch[1])
+  var uberPaidFloat = parseFloat(uberMatch[1])
+  if (actualFloat <= uberPaidFloat) 
+  {
+    setAcceptable("As best I can tell, you were paid fairly.", tabId);
+  }
+  else
+  {
+    setCheated("Uber paid you for " + uberPaidForDistance + " but you actually drove for " + actualDistance, tabId);
+  }
+}
+
+function callbackDirectionsComplete(response, status, uberPaidForDistance, tabId) {
+  setInfo("Directions request received from google.")
+
+  if (status !== 'OK') {
+    setError('Directions request failed due to ' + status);
+    return -1;
+  } else {
+    let directionsData = response.routes[0].legs[0]; // Get data about the mapped route
+    if (!directionsData) {
+      setError('Directions request failed');
+      return -1;
+    }
+    else {
+      // Success!
+      let actualDistance = directionsData.distance.text;
+      compareDistances(actualDistance, uberPaidForDistance, tabId)
+    }
+  }
+}
+
+function callbackFinishedReadingPage(tabId, dataFromContentScript) {
+  setIcon("loading128.gif", tabId)
+
+  let result = dataFromContentScript[0];
+  let pickupLatLon = result['pickupLatLon'];
+  let dropoffLatLon = result['dropoffLatLon'];
+  let uberPaidForDistance = result['uberPaidForDistance']
+  queryGoogleForDistance(pickupLatLon, dropoffLatLon, uberPaidForDistance, tabId);
+}
+
+function runCheatDetector(tabId) {
+  chrome.tabs.executeScript(
+      tabId,
+      {file: 'contentScript.js'},
+      function(result) {
+        callbackFinishedReadingPage(tabId, result);
+      }
+  )
+}
+
+chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
@@ -18,3 +163,24 @@ chrome.runtime.onInstalled.addListener(function() {
     }]);
   });
 });
+
+chrome.webNavigation.onCompleted.addListener(
+  function(details) {
+    if (!isGoogleAPILoaded)
+    {
+      setError("Please wait...the Google Maps API has not yet loaded")
+      return;
+    }
+
+    var tabId = details.tabId;
+    runCheatDetector(tabId)
+  }, {
+  url: [
+    {
+      hostSuffix: 'drivers.uber.com',
+      pathPrefix: '/p3/payments/v2/trips/',
+    }
+  ]}
+)
+
+addScriptTagToHead()
--- popup.js
+++ popup.js
@@ -1,130 +1,15 @@
-let changeColor = document.getElementById('changeColor');
-let bkg = chrome.extension.getBackgroundPage();
-
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-function getLatLonFor(pinImageSource, googleMapsImageSource) {
-  var numberRegex = "[-]?[0-9]*"
-  var latOrLonRegex = "(" + numberRegex + "." + numberRegex + ")"
-  var latAndLonRegex = latOrLonRegex + "%2C" + latOrLonRegex
-  var pickupRegex = new RegExp(pinImageSource + "%7Cscale%3A2%7C" + latAndLonRegex, "g");
-  var match = pickupRegex.exec(googleMapsImageSource)
-  var pickupLatitude = match[1]
-  var pickupLongitude = match[2]
-  return [pickupLatitude, pickupLongitude]
-}
-
-function isDistanceBetweenAccurate(startLatLon, endLatLon, uberPaidForDistance) {
-  bkg.console.log("Computing distance between coordinates " + startLatLon + " and " + endLatLon);
-
-  let directionsService = new google.maps.DirectionsService();
-  let start = new google.maps.LatLng(parseFloat(startLatLon[0]), parseFloat(startLatLon[1]));
-  let end = new google.maps.LatLng(parseFloat(endLatLon[0]), parseFloat(endLatLon[1]));
-
-  const route = {
-    origin: start,
-    destination: end,
-    travelMode: 'DRIVING'
-  }
-
-  directionsService.route(route, function(response, status) {
-    callbackDirectionsComplete(response, status, uberPaidForDistance);
+function setIcon(iconName, tabId) {
+  chrome.pageAction.setIcon({
+      path: "icons/" + iconName,
+      tabId: tabId
   });
 }
 
-function compareDistances(actualDistance, uberPaidForDistance) {
-  var mileageRegex = new RegExp("([0-9]*[.][0-9]*) (mi|km)", "g");
-  
-  // Get the uber match
-  var uberMatch = mileageRegex.exec(uberPaidForDistance);
-  // Reset regex
-  mileageRegex.lastIndex = 0;
-  // Get the actual match
-  var actualMatch = mileageRegex.exec(actualDistance);
-
-  // Error handling: This shouldn't happen.
-  if (!actualMatch || !uberMatch || actualMatch.length < 2 || uberMatch.length < 2)
-  {
-    window.alert(uberPaidForDistance)
-    window.alert("Could not parse mileages:\n" +
-                 "\nactual=" + actualDistance +
-                 "\nuber paid for=" + uberPaidForDistance +
-                 "\nactual match=" + actualMatch +
-                 "\nuber match=" + uberMatch);
-    return;
-  }
-
-  var actualFloat = parseFloat(actualMatch[1])
-  var uberPaidFloat = parseFloat(uberMatch[1])
-  if (actualFloat <= uberPaidFloat) 
-  {
-    window.alert("As best I can tell, you were paid fairly.");
-  }
-  else
-  {
-    window.alert("Uber paid you for " + uberPaidForDistance + " but you actually drove for " + actualDistance);
-  }
+function _setStatus(className, status) {
+  document.getElementById('status').className = className;
+  document.getElementById('status').innerHTML = status;
 }
 
-function callbackDirectionsComplete(response, status, uberPaidForDistance) {
-  bkg.console.log("Directions request received from google.")
-
-  if (status !== 'OK') {
-    window.alert('Directions request failed due to ' + status);
-    return -1;
-  } else {
-    var directionsData = response.routes[0].legs[0]; // Get data about the mapped route
-    if (!directionsData) {
-      window.alert('Directions request failed');
-      return -1;
-    }
-    else {
-      // Success!
-      actualDistance = directionsData.distance.text;
-      compareDistances(actualDistance, uberPaidForDistance)
-    }
-  }
-}
-
-var isLoadingComplete = false;
-window.googleAPiFinishedLoadingCallback = function() {
-  isLoadingComplete = true;
-}
-
-  changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      bkg.console.log("Trying to execute script");
-      if (!isLoadingComplete)
-      {
-        bkg.console.log("TODO not loaded yet- make a loading icon or sumn");
-        return;
-      }
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {file: 'contentScript.js'},
-          function(result) {
-            actualResult = result[0];
-            pickupLatLon = actualResult['pickupLatLon'];
-            dropoffLatLon = actualResult['dropoffLatLon'];
-            uberPaidForDistance = actualResult['uberPaidForDistance']
-            actualDistance = isDistanceBetweenAccurate(pickupLatLon, dropoffLatLon, uberPaidForDistance);
-          }
-      )
-    });
-  };
+chrome.storage.sync.get('popupStatus', function(data) {
+  _setStatus(data.popupStatus.className, data.popupStatus.text)
+});
